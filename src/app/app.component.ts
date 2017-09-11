25403a871a47cb5facf992f5dbbceda1f4f65442
Duy Nguyen <duy.nguyen@akqa.com>
2017-09-11 14:52:53 +1000

    add basic roi input.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  powerProducts = [
+    new PowerProduct('Product Bundle #1', 100),
+    new PowerProduct('Product Bundle #2', 200),
+    new PowerProduct('Product Bundle #3', 300),
+  ];
+  selectedProduct: PowerProduct;
+  employeeCount: number;
+  selectProduct(productIndex) {
+    console.log(productIndex);
+    this.selectedProduct = this.powerProducts[productIndex];
+  }
+  doCalculation() {
+    console.log(this.selectedProduct);
+    console.log(this.employeeCount);
+  }
+}
+
+class PowerProduct {
+  name: string;
+  price: number;
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
 }
